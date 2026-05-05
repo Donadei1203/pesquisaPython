b33c6959572cb8dcffaee69bba67b1ab9f651b9a
Donadei1203 <gu1203.doman@gmail.com>
2026-05-05 15:55:33 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+#Importando módulos
+from flask import Flask, render_template
+
+
+#iniciando o App
+app = Flask(__name__)
+
+
+#Criando a primeira rota
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
